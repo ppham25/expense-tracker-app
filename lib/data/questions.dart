5248389041c94aa9e8762e57900028fff427d54c
phuong <phuongptt.b23cn666@stu.ptit.edu.vn>
2026-02-25 00:07:42 +0700

    thêm dữ liệu câu hỏi

---
+++ lib/data/questions.dart
@@ -0,0 +1,28 @@
+import 'package:adv_basic/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('What\'s the capital of France?', [
+    'Paris la thu do cua nuoc y phai khong',
+    'London',
+    'Rome',
+    'Madrid',
+  ]),
+  QuizQuestion('What\'s the capital of Germany?', [
+    'Berlin',
+    'Vienna',
+    'Zurich',
+    'Hamburg',
+  ]),
+  QuizQuestion('What\'s the capital of Italy?', [
+    'Rome',
+    'Milan',
+    'Naples',
+    'Turin',
+  ]),
+  QuizQuestion('What\'s the capital of Spain?', [
+    'Madrid',
+    'Barcelona',
+    'Valencia',
+    'Seville',
+  ]),
+];
